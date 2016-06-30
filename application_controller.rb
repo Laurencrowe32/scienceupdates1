547e9cf4e90420dbcc7b4e4fb20bde43dd82e9b0
Lauren Crowe <laurencrowe32@yahoo.com>
2016-06-30 17:53:36 +0000

    sup

--- application_controller.rb
+++ application_controller.rb
@@ -1,9 +1,17 @@
 require 'bundler'
 Bundler.require
+require_relative 'models/model.rb'
 
 class MyApp < Sinatra::Base
 get '/' do
   erb :index
   #run bundle install and then shotgun
 end
-end 
+
+post '/results' do
+  print params
+  finding_links(params[:topics])
+#erb:
+end
+
+end
