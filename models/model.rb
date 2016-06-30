25237b3d0fa7e06259ffd75515b4a5efb33ca3a8
Lauren Crowe <laurencrowe32@yahoo.com>
2016-06-30 15:53:58 +0000

    sorta done

--- models/model.rb
+++ models/model.rb
@@ -1,19 +1,28 @@
-news = {astronomy: {name: "link"}, physics: "Physics", art3: "Medical", art4: "Biology" art5: "Chemistry", art6: "Computer Science" art7: "Ecology"}
+news = {
+  astronomy: [{name: "Astronomy", link: "http://www.astronomy.com/news"},{name: "Astronomy Now", link: "https://astronomynow.com/category/news/"}, {name: "Scientific American", link: "http://www.scientificamerican.com/space/"}],
+  physics: [{name: "Physics World", link: "http://physicsworld.com/cws/channel/news"}, {name: "Phys", link: "http://phys.org/physics-news/"}, {name: "Scientific American", link: "http://www.scientificamerican.com/physics/"}],
+  medical: [{name: "Medical News Bulletin", link: "http://www.medicalnewsbulletin.com"},{name: "Scientific American", link: "http://www.medicalnewsbulletin.com"},{name: "MNT", link: "http://www.medicalnewstoday.com"}],
+  biology: [{name: "Scientific American", link: "http://www.scientificamerican.com/biology/"}, {name: "Science Daily", link: "https://www.sciencedaily.com/news/plants_animals/biology/"}, {name: "Sci News", link: "http://www.sci-news.com/news/biology"}],
+  chemistry: [{name: "Scientific American", link: "http://www.scientificamerican.com/chemistry/"}, {name: "Science Daily", link: "https://www.sciencedaily.com/news/matter_energy/chemistry/"}, {name: "Chemistry World", link: "http://www.rsc.org/chemistryworld/"}],
+  computer_science: [{name: "Science Daily", link: "https://www.sciencedaily.com/news/computers_math/computer_science/"}, {name: "Phys", link: "http://phys.org/technology-news/computer-sciences/"}, {name: "Scientific American", link: "http://www.scientificamerican.com/computing/"}],
+  ecology: [{name: "Science Daily", link: "https://www.sciencedaily.com/news/earth_climate/ecology/"}, {name: "Ecology", link: "http://www.ecology.com/ecology-today/et-news/"}, {name: "Scientific American", link: "http://www.scientificamerican.com/ecology/"}]
+}
 
-def initialize(link)
-@link = link
+@subjects = ["astronomy", "physics"]
+
+@subjects.each do |subject|
+subject.to_sym
+done = news[subject.to_sym]
+done.each do |finish|
+  puts finish[:link]
+end
+end
+#array = [1, 2, 3, 4, 5, 6]
+#array.each { |x| puts x }
+def subject
+params[]
 end
 
-class art
-attr_reader :name, :link
-  def initialize(link)
-  @link = link
-  end
-end
 
-
-class User
-  attr_reader :email, :buttons
   #would i have to make an attraubte for every button as in art1 and it would be yes of no for each type of science
-end
   #how do i make it where i will make a new one for everyone who signs up
